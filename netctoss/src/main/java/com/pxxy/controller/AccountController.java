35770625398bb33be92c557ffbf5d4074a06e2c5
JiangCanOne <1101985696>
2019-05-29 17:14:30 +0800

    ok

--- netctoss/src/main/java/com/pxxy/controller/AccountController.java
+++ netctoss/src/main/java/com/pxxy/controller/AccountController.java
@@ -26,6 +26,7 @@ public class AccountController {
         List<Account> accounts = accountService.findAll();
         for (Account account : accounts){
             System.out.println(account);
+            System.out.println();
         }
         return "list";
     }
