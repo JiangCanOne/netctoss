4451ec9d0c06748e7606692d35b310a24711307a
JiangCanOne <1101985696>
2019-05-31 16:25:48 +0800

    ok

--- netctoss/src/main/java/com/pxxy/service/IBillService.java
+++
@@ -1,9 +0,0 @@
-package com.pxxy.service;
-
-/**
- * @Descricption:业务层账单接口
- * @Author:江灿
- * @Date:Create in 11:05 2019/5/29
- */
-public interface IBillService {
-}
--- netctoss/src/main/java/com/pxxy/service/IRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.pxxy.service;
-
-/**
- * @Descricption:业务层角色接口
- * @Author:江灿
- * @Date:Create in 11:09 2019/5/29
- */
-public interface IRoleService {
-}
